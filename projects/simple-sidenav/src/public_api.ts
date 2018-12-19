f439ab3d205c9183a7b6242f955196bcd13b1092
Igor Tikhonenko <itikhonenko.codica@gmail.com>
2018-12-19 14:37:29 +0200

    interface for simple menu

--- projects/simple-sidenav/src/public_api.ts
+++ projects/simple-sidenav/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of simple-sidenav
  */
 
-export * from './lib/simple-sidenav.service';
 export * from './lib/simple-sidenav.component';
 export * from './lib/simple-sidenav.module';
+export * from './lib/interfaces/simple-menu';
