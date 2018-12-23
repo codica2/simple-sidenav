9e259f4128d88570650ab6fcb35a73764560f35e
tihandjan <tihandjan198921@gmail.com>
2018-12-23 18:22:06 +0200

    [Fix] Fix animation module

--- projects/simple-sidenav/src/lib/simple-sidenav.module.ts
+++ projects/simple-sidenav/src/lib/simple-sidenav.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SimpleSidenavComponent } from './simple-sidenav.component';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [SimpleSidenavComponent],
   imports: [
-    CommonModule,
-    BrowserAnimationsModule
+    CommonModule
   ],
   exports: [SimpleSidenavComponent]
 })
